8f407a4df3c29a42f9f29701a38375b0bd09e47d
ShredderMing <eternallovext@gmail.com>
2017-09-17 20:09:01 +0800

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import babel from 'rollup-plugin-babel';
+
+let pkg = JSON.parse(fs.readFileSync('./package.json'));
+
+export default {
+  input: 'src/index.js',
+  output: { file: pkg.main, format: 'cjs' },
+  sourcemap: true,
+  name: pkg.amdName,
+  external: ['preact', 'aplayer'],
+  plugins: [
+    babel({
+      babelrc: false,
+      comments: false,
+      exclude: ['node_modules/**'],
+      presets: [['env', { modules: false, loose: true }]],
+      plugins: [
+        'transform-class-properties',
+        ['transform-react-jsx', { pragma: 'h' }]
+      ]
+    })
+  ]
+};
